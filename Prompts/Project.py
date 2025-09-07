8b01925d93065fb4ba40a6acfde74ed4dc3838ff
TanveerSingh31 <tanveersinghnayyar@gmail.com>
2025-09-07 17:44:51 +0530

    history in chat-bot

--- Prompts/Project.py
+++ Prompts/Project.py
@@ -2,16 +2,28 @@
 
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 
 
 load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY")
 
-llm = ChatGoogleGenerativeAI(model='gemini-2.5-pro', google_api_key=api_key)
+llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash', google_api_key=api_key)
+
+
+chat_history = [
+    SystemMessage("You are helpful AI assistant")
+]
+
 
 user_query = input("User: Enter prompt - ").lower()
 while(user_query != "quit"):
-    result = llm.invoke(user_query)
+    # add to chat history as HumanMessage
+    chat_history.append(HumanMessage(content=user_query))
+    result = llm.invoke(chat_history) # sending chat_history to LLM for each query
+
+    # add to chat history as AI Message
+    chat_history.append(AIMessage(content=result.content))
     print(f"AI: {result.content}")
-    user_query = input("Enter prompt").lower()
+    user_query = input("Enter prompt - ").lower()
